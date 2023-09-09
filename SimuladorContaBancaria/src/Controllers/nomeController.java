d9d652019e56022246b6b121e4d4cfebe608ded6
Lucas Al Alam <lucasalalam@protonmail.com>
2023-09-09 20:05:49 -0300

    feature: add name controller

---
+++ SimuladorContaBancaria/src/Controllers/nomeController.java
@@ -0,0 +1,30 @@
+package Controllers;
+import Exceptions.*;
+import Model.infoModel;
+
+public class nomeController {
+    static Model.infoModel data = new infoModel();
+    
+    public static void createNome (String nome) {
+        try {
+            validacaoNome(nome);
+            data.setNome(nome);
+        } catch (FormatoNomeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String showNome () {
+        return data.getNome();
+    }
+
+private static void validacaoNome(String nome) throws FormatoNomeException {
+    System.out.println(nome.length());
+        if (nome.length() <= 2) {
+            throw new FormatoNomeException("Insira um nome válido");
+        }
+        if (!nome.matches("^[a-zA-Z]*$")) {
+            throw new FormatoNomeException("O nome deve conter apenas letras");
+        }
+    }
+}
