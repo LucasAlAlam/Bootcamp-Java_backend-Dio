b726ac832de64003ac42b1d3f9b81821e91e6f3e
Lucas Al Alam <lucasalalam@protonmail.com>
2023-09-09 20:06:50 -0300

    feature: add model

---
+++ SimuladorContaBancaria/src/Model/infoModel.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class infoModel {
+    private String nome = new String();
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
