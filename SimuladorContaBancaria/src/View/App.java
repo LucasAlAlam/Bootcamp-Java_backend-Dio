4c36aa41198b6ab13182766dc6dd9605dc1d5c74
Lucas Al Alam <lucasalalam@protonmail.com>
2023-09-09 21:28:13 -0300

    feature: add better interaction with users

--- SimuladorContaBancaria/src/View/App.java
+++ SimuladorContaBancaria/src/View/App.java
@@ -7,13 +7,17 @@ public class App {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
         nomeController.createNome(input.nextLine());
+        System.out.println("Digite sua agência:");
         agenciaController.createAgencia(input.nextInt());
         input.nextLine(); //Limpeza de buffer
+        System.out.println("Digite o número da sua conta:");
         numeroController.createNumero(input.nextLine());
+        System.out.println("Digite seu saldo:");
         saldoController.createSaldo(input.nextDouble());
 
-        System.out.println("Olá "+nomeController.showNome()+". Sua conta de número "+numeroController.showNumero()+" na agência "+agenciaController.showAgencia()+" tem um saldo equivalente a R$"+saldoController.showSaldo()+".");
+        System.out.println("Olá, "+nomeController.showNome()+". Sua conta de número "+numeroController.showNumero()+" na agência "+agenciaController.showAgencia()+" tem um saldo equivalente a R$"+saldoController.showSaldo()+".");
         input.close();
     }
 }
